7076fc65ed3153de07930b019b1796ad23717256
Coleman Foley <colemanfoley@gmail.com>
2013-04-09 20:55:08 -0700

    Broke JavaScript out into its own file.

---
+++ app.js
@@ -0,0 +1,36 @@
+var Task = Backbone.Model.extend({
+  initialize: function(){
+    this.text = null;
+    this.completed = false;
+  }
+});
+
+var Tasks = Backbone.Collection.extend({model: Task});
+var MainView = Backbone.View.extend({
+  collection: Tasks,
+  initialize: function(){
+    this.render();
+  },
+  render: function(){
+    this.collection.each(function(task){
+      this.$el.append(new TaskView({model:task}).render());
+    }, this);
+  }
+});
+var TaskView = Backbone.View.extend({
+  model: Task,
+  initialize: function(){
+  },
+  render: function(){
+    return this.$el.html('<li class="task">'+this.model.get('text')+'</ul>');
+  }
+});
+
+var tasks = new Tasks([
+  new Task({text: "test", completed: true}),
+  new Task({text: "test2", completed: true})
+]);
+var mainView = new MainView({collection: tasks});
+$(document).ready(function(){
+  mainView.$el.appendTo(document.body);
+});
